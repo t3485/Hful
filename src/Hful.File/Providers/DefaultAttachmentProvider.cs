6387ee2c0b9d9c03f89cdae406ab3cb9dad1fa3b
sghmenbs <sghmenbs@163.com>
2023-03-30 19:13:56 +0800

    1

--- src/Hful.File/Providers/DefaultAttachmentProvider.cs
+++ src/Hful.File/Providers/DefaultAttachmentProvider.cs
@@ -1,6 +1,36 @@
-﻿namespace Hful.File.Providers
+﻿using Hful.File.Domain;
+
+namespace Hful.File.Providers
 {
-    internal interface DefaultAttachmentProvider
+    internal class DefaultAttachmentProvider : IAttachmentProvider
     {
+        public string Name => "Default";
+
+        public string Description => "";
+
+        public Stream Download(Attachment attachment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> DownloadAsync(Attachment attachment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAccessPath(Attachment attachment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Upload(Attachment attachment, Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadAsync(Attachment attachment, Stream stream)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
