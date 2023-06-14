c5a6052dda602413605da37c86df16d1c2b476b4
sghmenbs <sghmenbs@163.com>
2023-06-15 00:10:28 +0800

    1

---
+++ src/Hful.File.Job/CleanJob.cs
@@ -0,0 +1,24 @@
+﻿using Hful.File.Service;
+using Hful.Job;
+
+namespace Hful.File.Job
+{
+    public class CleanJob : HfulJob
+    {
+        public const string Key = "";
+        public const string Group = "";
+        public const string Cron = "";
+
+        private readonly IAttachmentService attachmentService;
+
+        public CleanJob(IAttachmentService attachmentService)
+        {
+            this.attachmentService = attachmentService;
+        }
+
+        public async override Task Execute(HfulJobContext context)
+        {
+
+        }
+    }
+}
---
+++ src/Hful.File.Job/FileJobModule.cs
@@ -0,0 +1,10 @@
+﻿using Hful.Core;
+using Hful.Job;
+
+namespace Hful.File.Job
+{
+    [HfulDependOn(typeof(FileModule), typeof(JobModule))]
+    public class FileJobModule : HfulModule
+    {
+    }
+}
